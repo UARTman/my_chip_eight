f49ad0540cf0a44f6ac67cb17a3ac03d62083ab9
uartman <uartman@mail.ru>
2020-05-24 19:58:12 +0300

    Tweak docs a bit more

--- src/emulator/mod.rs
+++ src/emulator/mod.rs
@@ -1,24 +1,26 @@
+/// Contains CHIP-8 Opcodes
 pub mod opcode;
 
 /// CHIP-8 Emulator
 pub struct Emulator {
     /// Represents CHIP-8 memory: 4096 bytes
     ///
-    /// TODO: Write memory locations
+    /// Bytes 0x000-0x200 - Interpreter space
+    /// Bytes 0x200-0xFFF - Working ROM and RAM
     pub memory: [u8; 4096],
     /// Represents CHIP-8 registers
     ///
     /// There are 16 registers
     ///
-    /// 16th register is used by system. TODO: Write more about its use.
+    /// 16th register is used as a carry flag
     pub registers: [u8; 16],
-    /// Address register
+    /// Memory address register
     pub index_register: u16,
     /// Program counter
     ///
-    /// TODO: Write more about it
+    /// Points to a current instruction
     pub program_counter: u16,
-    /// Stack for basic procedure calls
+    /// Stack for basic subroutine calls
     pub stack: Vec<u16>,
     /// Delay Timer
     pub delay_timer: u8,
