bddd69d6198f863a6d4e985afd3f79a6d5fccc18
uartman <uartman@mail.ru>
2020-05-25 14:50:53 +0300

    Document some operations + remove main from coverage

--- src/emulator/reg_ops.rs
+++ src/emulator/reg_ops.rs
@@ -1,18 +1,13 @@
 use crate::emulator::Emulator;
 
 impl Emulator {
+    /// Get register at `register`
     pub fn get_reg(&self, register: u8) -> u8 {
         self.registers[register as usize]
     }
 
+    /// Get register at `register` to `value`
     pub fn set_reg(&mut self, register: u8, value: u8) {
         self.registers[register as usize] = value
-    }
-
-    pub fn reg_op<T>(&mut self, register: u8, x: T)
-    where
-        T: Fn(&mut u8),
-    {
-        x(self.registers.get_mut(register as usize).unwrap())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[cfg_attr(tarpaulin, skip)]
+
 fn main() {
     println!("Hello, world!");
 }
